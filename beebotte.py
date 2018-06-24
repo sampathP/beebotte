c06240e53e293898d3a9164f71508f9b6522f49e
taaaaho <taaaaho@gmail.com>
2018-06-24 17:07:43 +0900

    add switchbot trigger

--- beebotte.py
+++ beebotte.py
@@ -1,10 +1,13 @@
 import paho.mqtt.client as mqtt
 import json
+import binascii
+from bluepy.btle import Peripheral
 
-TOKEN = "[Beebotteのトークン]"
+TOKEN = "Beebotteのトークン"
 HOST = "mqtt.beebotte.com"
-TOPIC = "[作成したチャンネル/リソース]"
+TOPIC = "作成したチャンネル/リソース"
 CACERT = "mqtt.beebotte.com.pem"
+SWITCHBOT = "SwitchbotのMACアドレス"
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -12,6 +15,11 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     print(msg.topic + " " + str(msg.payload))
+    p = Peripheral(SWITCHBOT, "random")
+	hand_service = p.getServiceByUUID("cba20d00-224d-11e6-9fb8-0002a5d5c51b")
+	hand = hand_service.getCharacteristics("cba20002-224d-11e6-9fb8-0002a5d5c51b")[0]
+	hand.write(binascii.a2b_hex("570100"))
+	p.disconnect()
 
 client = mqtt.Client()
 client.username_pw_set("token:%s"%TOKEN)
